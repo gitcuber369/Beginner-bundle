35c4862e1bf3bcf26b4e94f54d09d64096ba00ed
gitcuber369 <arpitchaudhary2003@gmail.com>
2024-04-10 20:47:38 +0530

    added pg and git

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react';
+
 
 function Github() {
+  const [query, setQuery] = useState('');
+  const [result, setResult] = useState([]);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`https://api.github.com/users/${query}`);
+      const data = await response.json();
+      setResult([data]);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setResult([]);
+    }
+  };
+
   return (
-    <div>Github</div>
-  )
+    <div className='text-center max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl" bg-gray-500/30'>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Enter the name of the user"
+          className="text-center m-2  rounded-md"
+        />
+        <button type="submit" className="text-white ml-2">
+          Search
+        </button>
+      </form>
+      {result.length > 0 && (
+        <div className="mt-4">
+          <h2 className="text-2xl text-white">Github Profile</h2>
+          <div className='text-white'>
+            <ul className="mt-4">
+              
+            <li className="text-red-500">Username: {result[0].login}</li>
+
+            <li><img src={result[0].avatar_url} alt="user_img" className="w-20 h-20 text-center" /></li>
+
+            <li>{result[0].name}</li>
+            <li>Company: {result[0].company || "none"} </li>
+            <li>Bio: {result[0].bio}</li>
+            <li>Public Repos: {result[0].public_repos}</li>
+            <li>Followers: {result[0].followers}</li>
+            <li>Following: {result[0].following}</li>
+          </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Github
+export default Github;
+
+
--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,13 +1,88 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState, useEffect, useRef } from 'react'
 
-function PasswordGenerator() {
-    const Generator = useCallback(() => {
-      let pass = ''
-      let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    })
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false); 
+  const [charAllowed, setcharAllowed] = useState(false)
+  const [Password, setPassword] = useState("");
+
+  const PasswordRef = useRef(null)
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"  
+    if(numberAllowed) {
+      str += "0123456789"
+    }
+    if(charAllowed) {
+      str += "!@#$%^&*()_+~`"
+    }
+    
+    for(let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length)
+      pass += str.charAt(char)
+    }
+
+    setPassword(pass)
+
+  }, [length, charAllowed, numberAllowed, setPassword])
+   
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
+  
+  const copypasswordtoClipboard = useCallback(() => {
+    PasswordRef.current?.select()
+    PasswordRef.current?.setSelectionRange(0);  
+    window.navigator.clipboard.writeText(Password)
+  }, [Password])
+
   return (
-    <div>Password Generator</div>
+    <>
+      <div >
+        <h1 >Password Generator</h1>
+      <div >
+        <input type="text" 
+        value = {Password}
+        placeholder='password'
+        readOnly
+        ref={PasswordRef}
+        />
+        <button onClick={copypasswordtoClipboard} >Copy</button>
+      </div>
+      <div >
+        <div>
+          <input type="range" min={8} max={80} value={length} className='cursor-pointer'
+          onChange={(e) => {setLength(e.target.value)}}
+          />
+          <label>Length:{length} </label>
+        </div>
+        <div>
+          <input 
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          id='numberInput'
+          onChange={() => {
+            setNumberAllowed((prev) => !prev);
+          }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div>
+          <input 
+          type="checkbox"
+          defaultChecked={charAllowed}
+          id='charInput'
+          onChange={() => {
+            setcharAllowed((prev) => !prev);
+          }}
+          />
+          <label htmlFor="charInput">Character</label>
+        </div>
+       </div>
+    </div>
+    </>   
   )
 }
 
-export default PasswordGenerator
+export default App
